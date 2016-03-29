982c0932405451da05c3a6070347a76f8d3f39a6
HelpShift <HelpShift@HelpShifts-MacBook-Pro.local>
2016-03-29 17:52:38 +0530

    Days 1-3

--- src/aoc/day2.clj
+++ src/aoc/day2.clj
@@ -4,6 +4,8 @@
 (defn part-1-input []
   (.trim (slurp "resources/day2-part1-input")))
 
+(def part-2-input part-1-input)
+
 (defn surface-area [[a b c]]
   (* 2 (+ (* a b)
           (* b c)
@@ -18,9 +20,27 @@
   (+ (surface-area dim)
      (slack dim)))
 
+(defn dims-from-input [input]
+  (let [dim-strs (s/split input #"\n")]
+    (for [dim-str dim-strs]
+      (map #(Integer. %)
+           (s/split dim-str #"x")))))
+
 (defn part-1 []
-  (let [dim-strs (s/split (part-1-input) #"\n")
-        dims (for [dim-str dim-strs]
-               (map #(Integer. %)
-                    (s/split dim-str #"x")))]
-    (reduce + (map paper-required-for-present dims))))
+  (reduce + (map paper-required-for-present
+                 (dims-from-input (part-1-input)))))
+
+(defn shortest-perimeter [dim]
+  (let [[a b] (sort dim)]
+    (* 2 (+ a b))))
+
+(defn volume [[a b c]]
+  (* a b c))
+
+(defn ribbon-required-for-present [dim]
+  (+ (shortest-perimeter dim)
+     (volume dim)))
+
+(defn part-2 []
+  (reduce + (map ribbon-required-for-present
+                 (dims-from-input (part-2-input)))))

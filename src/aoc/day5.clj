771b01829cceb8b26b6e7c8ea16ff95ae2917660
divyansh <divyansh.prakash@yahoo.com>
2016-03-30 17:06:45 +0530

    Day 5

--- src/aoc/day5.clj
+++ src/aoc/day5.clj
@@ -4,6 +4,8 @@
 (defn part-1-input []
   (.trim (slurp "resources/day5-part1-input")))
 
+(def part-2-input part-1-input)
+
 (defn has-at-least-three-vowels? [s]
   (let [letters (frequencies s)]
     (>= (+ (get letters \a 0)
@@ -29,10 +31,35 @@
        (has-no-taboos? s)))
 
 (defn strs-from-input [in]
-  (s/split in #"\n"))
+  (map #(.trim %)
+       (s/split in #"\n")))
 
 (defn part-1 []
   (->> (part-1-input)
        strs-from-input
        (filter nice?)
+       count))
+
+(defn has-at-least-one-repeated-letter-pair? [s]
+  (let [all-pairs (partition 2 1 s)
+        grouped-repeating-pairs (partition-by identity all-pairs)
+        non-repeating-pairs (mapcat #(take-nth 2 %)
+                                    grouped-repeating-pairs)
+        occurences (vals (frequencies non-repeating-pairs))]
+    ((comp not nil?) (some #(> % 1)
+                           occurences))))
+
+(defn has-at-least-one-repeated-letter-separated-by-1? [s]
+  ((comp not nil?) (some (fn [[a _ c]]
+                           (= a c))
+                         (partition 3 1 s))))
+
+(defn revised-nice? [s]
+  (and (has-at-least-one-repeated-letter-pair? s)
+       (has-at-least-one-repeated-letter-separated-by-1? s)))
+
+(defn part-2 []
+  (->> (part-2-input)
+       strs-from-input
+       (filter revised-nice?)
        count))
